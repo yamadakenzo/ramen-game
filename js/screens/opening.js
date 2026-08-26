5720cced06b7e12ae1c876f7a506ff75d72b16df
yamadakenzo <yamadakenzo@gmail.com>
2026-08-26 22:57:05 +0900

    noop check

--- js/screens/opening.js
+++ js/screens/opening.js
@@ -36,8 +36,8 @@ window.ScreenOpening = (function () {
   }
 
   // ---- 画像 ----
-  // js/asset-image.js と同じキャッシュ対策(?v=20260826135640)。tools/deploy-pages.sh が公開時に置換する。
-  var BUILD_V = "20260826135640";
+  // js/asset-image.js と同じキャッシュ対策(?v=20260826135705)。tools/deploy-pages.sh が公開時に置換する。
+  var BUILD_V = "20260826135705";
   var DIR = "img/opening/";
   function src(name) { return DIR + name + ".webp?v=" + BUILD_V; }
   // 画像の参照はこのテーブル1か所だけ。他所でファイル名を書かない。
